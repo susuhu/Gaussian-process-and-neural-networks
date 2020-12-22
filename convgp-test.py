ac3fd5a146f36d4993014b803984f691421a94e4
Susu Hu <since.1990@live.cn>
2020-12-22 05:40:25 +0100

    using gpflow convolution kernel

--- convgp-test.py
+++ convgp-test.py
@@ -9,11 +9,10 @@ from gpflow.utilities import print_summary, set_trainable
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-
 # Load and prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 # # plot images
@@ -46,16 +45,15 @@ t_test = y_test.reshape(NUM_TEST_DATA, -1).astype(np.float64)
 data = (x_train, y_train)
 test_data = (x_test, y_test)
 
-
 # GPflow
 f64 = lambda x: np.array(x, dtype=np.float64)
 max_abs_1 = lambda: tfp.bijectors.AffineScalar(shift=f64(-2.0), scale=f64(4.0))(
     tfp.bijectors.Sigmoid()
 )
 
-patch_shape = [3, 3]
+patch_shape = [5, 5]
 base_k = gpflow.kernels.SquaredExponential(variance=1., lengthscales=1.)
-conv_k = gpflow.kernels.Convolutional(base_k, IMAGE_SHAPE, patch_shape= [5, 5])
+conv_k = gpflow.kernels.Convolutional(base_k, IMAGE_SHAPE, patch_shape)
 conv_k.weights = gpflow.Parameter(conv_k.weights.numpy(), transform=max_abs_1())
 conv_f = gpflow.inducing_variables.InducingPatches(
     np.unique(conv_k.get_patches(x_train).numpy().reshape(-1, 25), axis=0)
@@ -89,7 +87,7 @@ res = gpflow.optimizers.Scipy().minimize(
 )
 train_acc = np.mean((conv_m.predict_y(x_train)[0] > 0.5).numpy().astype("float") == y_train)
 test_acc = np.mean((conv_m.predict_y(x_test)[0] > 0.5).numpy().astype("float") == y_test)
-print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc*100}%")
+print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc * 100}%")
 print("conv elbo after training: %.4e" % conv_elbo())
 
 res = gpflow.optimizers.Scipy().minimize(
@@ -100,7 +98,7 @@ res = gpflow.optimizers.Scipy().minimize(
 )
 train_acc = np.mean((conv_m.predict_y(x_train)[0] > 0.5).numpy().astype("float") == y_train)
 test_acc = np.mean((conv_m.predict_y(x_test)[0] > 0.5).numpy().astype("float") == y_test)
-print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc*100}%")
+print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc * 100}%")
 print("conv elbo after training: %.4e" % conv_elbo())
 
 set_trainable(conv_m.kernel.weights, True)
@@ -112,7 +110,7 @@ res = gpflow.optimizers.Scipy().minimize(
 )
 train_acc = np.mean((conv_m.predict_y(x_train)[0] > 0.5).numpy().astype("float") == y_train)
 test_acc = np.mean((conv_m.predict_y(x_test)[0] > 0.5).numpy().astype("float") == y_train)
-print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc*100}%")
+print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc * 100}%")
 print("conv elbo after training: %.4e" % conv_elbo())
 
 gpflow.utilities.print_summary(conv_m)
