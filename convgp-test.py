d61981af7af09b826029942b36ef181237bec720
Susu Hu <since.1990@live.cn>
2021-01-16 15:19:48 +0100

    convgp

--- convgp-test.py
+++ convgp-test.py
@@ -24,16 +24,17 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 # set numbers
-NUM_TRAIN_DATA = np.array([60, 600, 6000, 60000])
+NUM_TRAIN_DATA =np.array([60, 600, 6000, 60000])
 NUM_TEST_DATA = x_test.shape[0]
-MAXITER = 100
+MAXITER = 1000
 H = W = 28  # width and height
 IMAGE_SHAPE = [H, W]
+patch_shape = [5, 5]
 
 # different training data size run
 for num in NUM_TRAIN_DATA:
-    x_train = x_train[0:num]  # (n,28,28)
-    y_train = y_train[0:num]  # (n,)
+    x_train = x_train[0: num]  # (n,28,28)
+    y_train = y_train[0: num]  # (n,)
 
     # for gpflow function data and test_data
     x_train = x_train.reshape(num, -1).astype(np.float64)  # (n, 28*28)
@@ -43,43 +44,52 @@ for num in NUM_TRAIN_DATA:
     data = (x_train, y_train)
     test_data = (x_test, y_test)
 
-    # GPflow
-    f64 = lambda x: np.array(x, dtype=np.float64)
-    max_abs_1 = lambda: tfp.bijectors.AffineScalar(shift=f64(-2.0), scale=f64(4.0))(
-        tfp.bijectors.Sigmoid()
-    )
+    # constrains lambda functions
+    # f64 = lambda x: np.array(x, dtype=np.float64)
+    # positive_with_min = lambda: tfp.bijectors.AffineScalar(shift=f64(1e-4))(tfp.bijectors.Softplus())
+    # constrained = lambda: tfp.bijectors.AffineScalar(shift=f64(1e-4), scale=f64(100.0))(
+    #     tfp.bijectors.Sigmoid()
+    # )
+    # max_abs_1 = lambda: tfp.bijectors.AffineScalar(shift=f64(-2.0), scale=f64(4.0))(
+    #     tfp.bijectors.Sigmoid()
+    # )
 
-    patch_shape = [5, 5]
     # base kernel
     base_k = gpflow.kernels.SquaredExponential()
     # conv kernel by sum all the patches
     conv_k = gpflow.kernels.Convolutional(base_k, IMAGE_SHAPE, patch_shape)
-    conv_k.weights = gpflow.Parameter(conv_k.weights.numpy(), transform=max_abs_1())
+    # base_k.lengthscales = gpflow.Parameter(1.0, transform=positive_with_min())
+    # # Weight scale and variance are non-identifiable. We also need to prevent variance from shooting off crazily.
+    # conv_k.base_kernel.variance = gpflow.Parameter(1.0, transform=constrained())
+    # conv_k.weights = gpflow.Parameter(conv_k.weights.numpy(), transform=max_abs_1())
     conv_f = gpflow.inducing_variables.InducingPatches(
         np.unique(conv_k.get_patches(x_train).numpy().reshape(-1, 25), axis=0)
     )
 
-    # sparse variance Gaussian Process
-    conv_m = gpflow.models.SVGP(conv_k, gpflow.likelihoods.Bernoulli(), conv_f)
+    # sparse variance Gaussian Process num_data=minibatch
+    conv_m = gpflow.models.SVGP(conv_k, gpflow.likelihoods.MultiClass(10), conv_f, num_latent_gps=10)
 
     conv_training_loss_closure = conv_m.training_loss_closure(data, compile=True)
-    conv_elbo = lambda: -conv_training_loss_closure().numpy()
-    print("conv elbo before training: %.4e" % conv_elbo())
+    conv_elbo = conv_m.elbo(data)
+    print("conv elbo before training: %.4e" % conv_elbo)
     print("conv model summary before training:")
     gpflow.utilities.print_summary(conv_m)
 
     print("TRAINING DATA SIZE IS %d" % num)
 
-    # set variance, weight as trainable parameters
-    set_trainable(conv_m.kernel.base_kernel.variance, True)
-    set_trainable(conv_m.kernel.weights, True)
+    # set variance, lengthscale, weight as trainable parameters
+    set_trainable(conv_m.kernel.base_kernel.variance, True)  # SquaredExponential variance
+    set_trainable(conv_m.kernel.base_kernel.lengthscales, True)  # SquaredExponential lengthscales
+    set_trainable(conv_m.kernel.weights, True)  # conv kernel weights
     res = gpflow.optimizers.Scipy().minimize(
         conv_training_loss_closure,
         variables=conv_m.trainable_variables,
         method="l-bfgs-b",
         options={"disp": True, "maxiter": MAXITER},
     )
+
     # results of training variance
+    # conv_m.predict_y(x_train)[0] = mean, conv_m.predict_y(x_train)[1] = variance
     train_acc = np.mean((conv_m.predict_y(x_train)[0] > 0.5).numpy().astype("float") == y_train)
     test_acc = np.mean((conv_m.predict_y(x_test)[0] > 0.5).numpy().astype("float") == y_test)
     print(f"Train acc: {train_acc * 100}%\nTest acc : {test_acc * 100}%")
